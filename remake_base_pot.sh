46aa178ea785594476c30bd7e8ff24a1be9bc1d5
trigg <triggerhapp@gmail.com>
2022-07-19 22:47:19 +0100

    - Translation update - To Native Welsh speakers:   I profusely apologise for   butchering the language

--- remake_base_pot.sh
+++ remake_base_pot.sh
@@ -2,3 +2,11 @@
 
 xgettext -d base --no-location -o discover_overlay/locales/base.pot discover_overlay/*.py discover_overlay/glade/settings.glade
 sed -i 's/charset=CHARSET/charset=UTF-8/g' discover_overlay/locales/base.pot
+
+# Update all .po files with new keys
+
+for dir in `find discover_overlay/locales/ -mindepth 1 -maxdepth 1 -type d`; do
+    touch "${dir}/LC_MESSAGES/default.po"
+    msgmerge -N ${dir}/LC_MESSAGES/default.po discover_overlay/locales/base.pot > ${dir}/LC_MESSAGES/default.po.new
+    mv ${dir}/LC_MESSAGES/default.po.new ${dir}/LC_MESSAGES/default.po
+done
