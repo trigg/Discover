e48fecaf74e51d396dd0a0f6e5204f9d68ebe4ba
trigg <triggerhapp@gmail.com>
2024-04-03 22:07:16 +0100

    - Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='discover-overlay',
     author='trigg',
     author_email='',
-    version='0.7.2',
+    version='0.7.3',
     description='Voice chat overlay',
     long_description=readme(),
     long_description_content_type='text/markdown',
